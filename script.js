b1e1006a4c0ccf6dd8aeee52759148a692f982dd
chinmaym07 <b418020@iiit-bh.ac.in>
2020-12-09 02:32:17 +0530

    Changes Done

--- script.js
+++ script.js
@@ -19,21 +19,30 @@ function setTheme(mode) {
     if (mode == "light") {
         document.getElementById("theme-style").href = "default.css";
         document.getElementById('github').style.fill="black";
+        document.getElementById('codesandbox').style.fill="black";
+        document.getElementById('codepen').style.fill="black";
     }
 
     if (mode == "blue") {
         document.getElementById("theme-style").href = "blue.css";
         document.getElementById('github').style.fill="white";
+        document.getElementById('codesandbox').style.fill="white";
+        document.getElementById('codepen').style.fill="white";
     }
 
     if (mode == "green") {
         document.getElementById("theme-style").href = "green.css";
         document.getElementById('github').style.fill="black";
+        document.getElementById('codesandbox').style.fill="black";
+        document.getElementById('codepen').style.fill="black";
     }
 
     if (mode == "purple") {
         document.getElementById("theme-style").href = "purple.css";
         document.getElementById('github').style.fill="white";
+        document.getElementById('codesandbox').style.fill="black";
+        document.getElementById('codepen').style.fill="white";
+        
     }
 
     localStorage.setItem("theme", mode);
